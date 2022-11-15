b2a9f64bdd1161750168237f91c3f6eb915c6321
lakshmanK1 <113410820+lakshmanK1@users.noreply.github.com>
2022-11-15 19:17:13 +0530

    added the functionality to  store current state, by using local storage

--- AuthContext.js
+++ AuthContext.js
@@ -3,16 +3,19 @@ import React ,{useState}from "react";
 export const AuthContext = React.createContext();
 
 const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const localStorageValues = localStorage.getItem('token');
+    const [token, setToken] = useState(localStorageValues);
 
     const userIsLogedIn = !!token;
 
     const loginHandler = (token) => {
         setToken(token);
+        localStorage.setItem('token', token);
     }
 
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     }
 
     const ContextValue = {
@@ -21,6 +24,7 @@ const AuthContextProvider = (props) => {
         LogIn:loginHandler,
         LogOut:logoutHandler
     }
+
     return (
         <AuthContext.Provider value={ContextValue}>
             {props.children}
