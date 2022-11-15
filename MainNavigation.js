8795bf974a1438011e31f507be3d84b53ca24e40
lakshmanK1 <113410820+lakshmanK1@users.noreply.github.com>
2022-11-15 15:08:34 +0530

    added functionality when to show profile, login, logout buttons

---
+++ MainNavigation.js
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Store/Context';
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  const {isLoggedIn} = useContext(AuthContext);
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isLoggedIn && 
+          <li>
+          <Link to='/auth'>Login</Link>
+        </li>
+          }
+          {isLoggedIn && 
+          <li>
+          <Link to='/profile'>Profile</Link>
+        </li>
+          }
+
+          {
+            isLoggedIn && 
+            <li>
+            <button>Logout</button>
+          </li>
+          }
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
